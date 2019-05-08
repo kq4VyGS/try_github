ac0aac506adc4e2509bc7d24dab534830afe8f62
kq4VyGS <arakaki1048@occ.co.jp>
2019-05-08 12:08:05 +0900

    addクラス作成

---
+++ ConsoleApp1/ConsoleApp1/Add.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Add
+    {
+        private int num;
+        public int add(int a, int b)
+        {
+            num = a + b;
+            return num;
+        }
+    }
+}
